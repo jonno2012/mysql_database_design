7cbb561e8053be688d1595d9e4c7ed7db072a86b
jonno2012 <jonathanrhill78@gmail.com>
2023-03-04 16:53:48 +0000

    Many to Many

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Post extends Model
+class Tag extends Model
 {
     use HasFactory;
 
-    public function Tag(): BelongsToMany
+    public function post(): BelongsToMany
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Post::class);
     }
 }
